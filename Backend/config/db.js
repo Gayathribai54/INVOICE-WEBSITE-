78f3c03155c01f9bd0806ef03eec79e5b491a574
Gayathribai54 <gayathrithakur95@gmail.com>
2026-01-31 04:39:40 -0800

    update

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed", error);
