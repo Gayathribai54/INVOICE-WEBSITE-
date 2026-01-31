5797e70a5eb6c13feb09ae852ec682ba1232ffda
Gayathribai54 <gayathrithakur95@gmail.com>
2026-01-31 03:57:25 -0800

    update

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -4,7 +4,12 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (invoice) => {
+  console.log(process.env.EMAIL);
+  console.log(process.env.EMAIL_PASS);
   const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // MUST be false for 587
     service: "gmail",
     auth: {
       user: process.env.EMAIL,
@@ -24,6 +29,7 @@ const sendEmail = async (invoice) => {
       },
     ],
   });
+  console.log("sent email successfully")
 };
 
 export default sendEmail;
